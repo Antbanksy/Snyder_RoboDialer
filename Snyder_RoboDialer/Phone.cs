4f0afb6e39827a1323e9033c70904b466dd4fb5c
antba <antba@DESKTOP-SVL14FT>
2023-05-25 08:32:14 -0700

    Add project files.

---
+++ Snyder_RoboDialer/Phone.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snyder_RoboDialer
+{
+    public abstract class Phone
+    {
+        protected string phoneNumber;
+        protected string companyName;
+
+        public Phone(string phoneNumber, string companyName)
+        {
+            this.phoneNumber = phoneNumber;
+            this.companyName = companyName;
+        }
+
+        public abstract string Dial();
+    }
+
+    public class HomePhone : Phone
+    {
+        public HomePhone(string phoneNumber, string companyName) : base(phoneNumber, companyName)
+        {
+        }
+
+        public override string Dial()
+        {
+            string formattedNumber = FormatPhoneNumber(phoneNumber);
+            return $"Dialing {companyName} using {formattedNumber}...";
+        }
+
+        private string FormatPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.Replace("(", "").Replace(")", "").Replace(" ", "");
+        }
+    }
+
+    public class CellPhone : Phone
+    {
+        public CellPhone(string phoneNumber, string companyName) : base(phoneNumber, companyName)
+        {
+        }
+
+        public override string Dial()
+        {
+            string formattedNumber = FormatPhoneNumber(phoneNumber);
+            string dialingCode = "1+";
+            return $"Dialing {companyName} using {dialingCode}{formattedNumber}...";
+        }
+
+        private string FormatPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.Replace("(", "").Replace(")", "").Replace(" ", "");
+        }
+    }
+
+
+}
---
+++ Snyder_RoboDialer/Program.cs
@@ -0,0 +1,32 @@
+﻿// Anthony Snyder
+// IT112
+// NOTES: I am so tired
+// BEHAVIORS NOT IMPLIMENTED AND WHY: Did not have much time as i am at track state,
+// but there is no excuses, i did not get this program to completely work.
+namespace Snyder_RoboDialer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Phone[] dialingList = new Phone[10];
+                dialingList[0] = new HomePhone("(303) 985-5060", "CompuTest");
+                dialingList[1] = new CellPhone("(603) 532-4123", "Curtis Manufacturing");
+                dialingList[2] = new HomePhone("(800) 876-2524", "Data Functions");
+                dialingList[3] = new HomePhone("(708) 397-3330", "Donnay Repair");
+                dialingList[4] = new HomePhone("(360) 434-3894", "ErgoNomic Inc");
+                dialingList[5] = new HomePhone("(800) 969-4374", "ErgoSource");
+                dialingList[6] = new CellPhone("(800) 874-8527", "Fox Bay Industries");
+                dialingList[7] = new CellPhone("(800) 545-6254", "Glare-Guard");
+                dialingList[8] = new CellPhone("(407) 783-6641", "Hazard Comm Specialists");
+                dialingList[9] = new CellPhone("(714) 472-4409", "Komfort Support");
+
+                foreach (Phone phone in dialingList)
+                {
+                    Console.WriteLine(phone.Dial());
+                }
+            }
+        }
+    }
+}
